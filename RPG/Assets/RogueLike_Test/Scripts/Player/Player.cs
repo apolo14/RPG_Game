8d70529be25047982d339687fecc746d67cc6ddb
apolo14 <felipe_cenarios@hotmail.com>
2019-10-20 15:14:47 -0300

    roqueLike

---
+++ RPG/Assets/RogueLike_Test/Scripts/Player/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float moveSpeedSlow = 2.5f;
+
+    public Rigidbody2D rb;
+    public Camera cam;
+
+    Vector2 movement;
+    Vector2 mousePos;
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void FixedUpdate()
+    {
+        //rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        MovementO();
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90; //usar "-90f" isso serve para posicionar a direção do personagem com o mouse
+        rb.rotation = angle;
+    }
+
+    void MovementO()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            rb.MovePosition(rb.position + movement * moveSpeedSlow * Time.fixedDeltaTime);
+        }
+        else
+        {
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        }
+    }
+}
+
+
+
+
